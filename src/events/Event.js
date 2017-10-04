440d58b97a83a8d53ac87ff336b442846458d29e
psykrsna <saivnm5@gmail.com>
2017-10-04 23:11:47 +0530

    Adding GA event to track outbound links

--- src/events/Event.js
+++ src/events/Event.js
@@ -1,32 +1,46 @@
 import React, { Component } from 'react';
 
 class Event extends Component {
-  render() {
-    let media = null;
-    if(this.props.data.mediaIsVideo){
-        media = <video autoPlay loop><source src={this.props.data.media} type="video/mp4" /></video>;
-    }
-    else{
-        media =  <img src={this.props.data.media} alt="event's name" />;
+
+    handleOutboundLink = (url) => {
+        debugger
+        window.ga('send', 'event', {
+            eventCategory: 'Outbound Link',
+            eventAction: 'click',
+            eventLabel: url
+        });
     }
 
-    return (
-        <div className="event">
-            <div className="event-media">
-                {media}
-            </div>
-            <div className="event-content">
-                <div className="font-heading">
-                    <a href={this.props.data.link} target="_blank">
-                        {this.props.data.hashtag}
-                    </a>
+    render() {
+        let media = null;
+        if(this.props.data.mediaIsVideo){
+            media = <video autoPlay loop><source src={this.props.data.media} type="video/mp4" /></video>;
+        }
+        else{
+            media =  <img src={this.props.data.media} alt="event's name" />;
+        }
+
+        return (
+            <div className="event">
+                <div className="event-media">
+                    {media}
                 </div>
-                <div className="font-sub-heading">{this.props.data.location}</div>
-                <div className="font-sub-heading">{this.props.data.date}</div>
+                <div className="event-content">
+                    <div className="font-heading">
+                        <a
+                            href={this.props.data.link}
+                            target="_blank"
+                            onClick={() => this.handleOutboundLink(this.props.data.link)}
+                        >
+                            {this.props.data.hashtag}
+                        </a>
+                    </div>
+                    <div className="font-sub-heading">{this.props.data.location}</div>
+                    <div className="font-sub-heading">{this.props.data.date}</div>
+                </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default Event;
