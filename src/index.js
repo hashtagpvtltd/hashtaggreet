86ae77ca6a5eb4e775ba814432256061dda2e167
psykrsna <saivnm5@gmail.com>
2017-10-05 18:42:58 +0530

    HTML restructuring fix

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ class Routes extends React.Component{
   render() {
     return (
       <Router>
-        <div class="app-container">
+        <div class="pseudo-root">
           <Route exact path="/" component={() => (<App type="events" />)} />
           <Route exact path="/stories" component={() => (<App type="stories" />)} />
         </div>
